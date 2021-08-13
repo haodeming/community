39a8f393514b9d6f57f7b7382c7f8a5231c5a435
haodeming <1986107751@qq.com>
2021-08-13 21:01:35 +0800

    int repo

---
+++ src/test/java/life/majiang/community/communitydemo/CommunitydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package life.majiang.community.communitydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunitydemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
