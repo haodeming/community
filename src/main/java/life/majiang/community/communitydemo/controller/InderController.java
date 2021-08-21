4956f99f6f32962d78d33ec625f15eed2a4e4049
haodeming <1986107751@qq.com>
2021-08-21 20:37:10 +0800

    -2021/8/21

--- src/main/java/life/majiang/community/communitydemo/controller/InderController.java
+++ src/main/java/life/majiang/community/communitydemo/controller/InderController.java
@@ -1,15 +1,38 @@
 package life.majiang.community.communitydemo.controller;
 
+import life.majiang.community.communitydemo.mapper.UserMapper;
+import life.majiang.community.communitydemo.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 public class InderController {
 
+    @Autowired
+    private UserMapper userMapper;
+
+
+
     @GetMapping("/")
-    public String hello(){
+    public String hello(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies){
+            if(cookie.getName().equals("token")){
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if(user != null){
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
         return "index";
     }
 
